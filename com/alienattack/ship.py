0336abc785f0b0da7c20680d3ecb5cc237ba7f8a
Manauwar Ansari <manauwar.aws@gmail.com>
2024-01-13 20:46:40 +0530

    Added Ship Settings

---
+++ com/alienattack/ship.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Ship:
+    def __init__(self,aa_game):
+        """Initialize the ship and set its starting position"""
+        self.screen = aa_game.screen
+        self.screen_rect = aa_game.screen.get_rect()
+
+        #load the ship image in the images folder
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        #start new ship from the botton center of screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current position"""
+        self.screen.blit(self.image,self.rect)
