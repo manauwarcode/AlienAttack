ce2d73182dad7bf76b069d539a9310a5c4bf9dfc
Manauwar Ansari <manauwar.aws@gmail.com>
2024-01-13 20:25:01 +0530

    Added Frame rate and background color

--- com/alienattack/alien_attack.py
+++ com/alienattack/alien_attack.py
@@ -4,15 +4,20 @@ import pygame
 class AlienInvasion():
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Attack")
+        self.bg_color = (230,230,230)
+
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == "__main__":
     aa = AlienInvasion()
