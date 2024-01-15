9b33819852b05542bedf28521ed994979da37ce8
Manauwar Ansari <manauwar.aws@gmail.com>
2024-01-15 20:14:10 +0530

    defined boundry of ship

--- com/alienattack/ship.py
+++ com/alienattack/ship.py
@@ -26,9 +26,9 @@ class Ship:
         self.moving_down = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         if self.moving_up:
             self.y -= self.settings.ship_speed
