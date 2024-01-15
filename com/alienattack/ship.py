7ad265cdb558dc14e5391e3f12fa39fdc6dea65a
Manauwar Ansari <manauwar.aws@gmail.com>
2024-01-15 19:54:49 +0530

    Increased the speed of ship

--- com/alienattack/ship.py
+++ com/alienattack/ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, aa_game):
         """Initialize the ship and set its starting position"""
         self.screen = aa_game.screen
+        self.settings = aa_game.settings
         self.screen_rect = aa_game.screen.get_rect()
 
         # load the ship image from the images folder
@@ -14,7 +15,11 @@ class Ship:
         # start new ship from the bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
 
-        #Movement Flag
+        #store the float for ship exact position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement Flag
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
@@ -22,13 +27,17 @@ class Ship:
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
         if self.moving_up:
-            self.rect.y -= 1
+            self.y -= self.settings.ship_speed
         if self.moving_down:
-            self.rect.y +=1
+            self.y += self.settings.ship_speed
+
+        #update rect object from self.x
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current position"""
