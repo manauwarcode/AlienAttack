a45a0b58ab943dde2105f50dae5fda1f7733ed6f
Manauwar Ansari <manauwar.aws@gmail.com>
2024-01-16 13:56:23 +0530

    added the code the fire the bullet

--- com/alienattack/alien_attack.py
+++ com/alienattack/alien_attack.py
@@ -10,7 +10,7 @@ class AlienInvasion():
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Attack")
@@ -42,6 +42,8 @@ class AlienInvasion():
                     self.ship.moving_down = True
                 if event.key == pygame.K_q:
                     sys.exit()
+                if event.key == pygame.K_SPACE:
+                    self._fire_bullet()
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     self.ship.moving_right = False
@@ -51,8 +53,15 @@ class AlienInvasion():
                     self.ship.moving_up = False
                 if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     self.ship.moving_down = False
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet(quit(quit()))
         self.ship.blitme()
         pygame.display.flip()
 
